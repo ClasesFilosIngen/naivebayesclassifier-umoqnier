ef6b8e8bfbf71fbfe272c707dbda4db9b687ca26
Diego Alberto Barriga Martínez <diegobarriga@protonmail.com>
2019-02-27 23:51:37 -0600

    [ADD] Docstrings y ToDos

--- apriori.py
+++ apriori.py
@@ -2,9 +2,14 @@
 import itertools
 
 
-def get_elementos_candidatos(trans):
+def candidatos_iniciales(base_datos):
+    """
+    Función que obtiene los candidatos iniciales que serán singletones
+    :param base_datos: Datos a ser analizados por el algoritmo
+    :return: Lista de singletones que serán los candidatos iniciales
+    """
     data = []
-    for t in trans:
+    for t in base_datos:
         for item in t:
             if item not in data:
                 data.append(item)
@@ -12,6 +17,12 @@ def get_elementos_candidatos(trans):
 
 
 def generador_candidatos(frecuentes, k):
+    """
+    Función encargada de generar candidatos a ser frecuentes
+    :param frecuentes: Elemento frecuentes de la iteración k
+    :param k: Iteración k
+    :return: Lista de tuplas que serán elementos candidatos a ser frecuentes
+    """
     if k == 2:
         data = list(itertools.combinations(frecuentes, 2))
         return [tuple(sorted(d)) for d in data]
@@ -26,19 +37,34 @@ def generador_candidatos(frecuentes, k):
         return data
 
 
-def get_elementos_frecuentes(singletones, transiciones, min_sup):
+def frecuentes_iniciales(singletones, base_datos, min_sup):
+    """
+    Obtiene los elementos frecuentes con base en los singletones iniciales de la base
+    :param singletones: Candidatos de 1 elemento
+    :param base_datos: Base de elemento a ser analizados
+    :param min_sup: Soporte mínimo arbitrario
+    :return: Lista de singletones frecuentes en la base de datos
+    """
     elementos = list()
     for singleton in singletones:
         cuenta = 0
-        for t in transiciones:
+        for t in base_datos:
             if singleton in t:
                 cuenta += 1
-        if (cuenta / len(transiciones)) >= min_sup:
+        if (cuenta / len(base_datos)) >= min_sup:
             elementos.append(singleton)
     return elementos
 
 
 def descartar_candidatos(candidatos, previos_frecuentes, k):
+    """
+    Función encargada de descartar candidatos frecuentes si alguna combinación de la iteración k - 1 elementos
+    no pertenece a los elementos frecuentes previos.
+    :param candidatos: Candidatos generados en la iteración k
+    :param previos_frecuentes: Elementos frecuentes de la iteración k - 1
+    :param k: K en turno
+    :return: Lista de tuplas que son los elementos candidatos que cumplen condición de pertenencia
+    """
     data = list()
     for candidato in candidatos:
         flag = 0
@@ -52,9 +78,16 @@ def descartar_candidatos(candidatos, previos_frecuentes, k):
     return data
 
 
-def apriori(transiciones, min_sup, min_conf):
-    candidatos = get_elementos_candidatos(transiciones)
-    frecuentes = get_elementos_frecuentes(candidatos, transiciones, min_sup)
+def apriori(base_datos, min_sup, min_conf):
+    """
+    Algoritmo apriori para obtener items frecuentes en una base de datos
+    :param base_datos: Datos que serán analizados. Ej: carrito de compras
+    :param min_sup: Soporte mínimo
+    :param min_conf: Valor mínimo de confianza
+    :return: Lista de tuplas con los elementos frecuentes
+    """
+    candidatos = candidatos_iniciales(base_datos)
+    frecuentes = frecuentes_iniciales(candidatos, base_datos, min_sup)
     previos_frecuentes = list()
     k = 2
     while len(frecuentes) != 0:
@@ -62,7 +95,7 @@ def apriori(transiciones, min_sup, min_conf):
         candidatos_k = generador_candidatos(frecuentes, k)
         if k > 2:
             candidatos_k = descartar_candidatos(candidatos_k, frecuentes, k)
-        for t in transiciones:
+        for t in base_datos:
             for c in candidatos_k:
                 if frozenset(c).issubset(set(t)):
                     if c not in c_dict.keys():
@@ -71,12 +104,14 @@ def apriori(transiciones, min_sup, min_conf):
                         c_dict[c] += 1
         previos_frecuentes += frecuentes[:]
         frecuentes = [relacion for relacion, frecuencia in zip(c_dict.keys(), c_dict.values())
-                             if (frecuencia / len(transiciones)) >= min_sup]
+                      if (frecuencia / len(base_datos)) >= min_sup]
         k += 1
     return previos_frecuentes
 
 
 if __name__ == '__main__':
+    # TODO: Falta probar con casos diversos
+    # TODO: Refactorizar funciones
     carrito = (("carne", "pollo", "leche"), ("carne", "queso"), ("queso", "botas"),
              ("carne", "pollo", "queso"), ("carne", "pollo", "ropa", "queso", "leche"),
              ("pollo", "ropa", "leche"), ("pollo", "leche", "ropa"))
